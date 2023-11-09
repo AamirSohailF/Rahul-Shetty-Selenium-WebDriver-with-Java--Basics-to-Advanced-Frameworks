3ddb54bb5d05a3a152f1e2f92b7dfc29637fcdc1
Aamir Sohail <aso@leapwork.com>
2023-11-10 02:13:59 +0530

    2nd push

---
+++ pjav/src/pjav/StringReversal.java
@@ -0,0 +1,36 @@
+package pjav;
+
+public class StringReversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "Automation";
+		
+		
+//		Method 1 using String Builder
+		
+		StringBuilder s = new StringBuilder();
+		
+		s = s.append(str);
+		s = s.reverse();
+		System.out.print("From method 1: " + s);
+		
+		
+		System.out.println();
+//		Method 2 using loop
+		
+		System.out.print("From method 2: ");
+		
+		char [] c = str.toCharArray();
+		
+		for (int i = c.length-1; i >= 0; i--) {
+			System.out.print(c[i]);
+		}
+		
+		
+		
+		
+	}
+
+}
